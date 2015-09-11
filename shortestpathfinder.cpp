3fe3a5ff0e11911e5cccd0d1bc50f4aff1506c59
Hemachandar <hemachandarmit@gmail.com>
2015-09-11 22:15:54 +0530

    New Features

--- shortestpathfinder.cpp
+++ shortestpathfinder.cpp
@@ -2,8 +2,10 @@
 #include<cstring>
 #include<limits>
 #include<windows.h>
+#include<conio.h>
 #include<dos.h>
 #include<stack>
+
 #define I 1000
 #define V 10
 using namespace std;
@@ -11,67 +13,67 @@ using namespace std;
 int weight[V];
 
 const string str[]={
-"NEWSSTAND"
-, "MATHS"
-, "REFERENCE STACK"
-, "FICTION"
-, "BIOLOGY"
-, "GENERAL KNOWLEDGE"
-, "PHYSICS"
-, "CHEMISTRY"
-, "AERONAUTICS"
-, "THERMODYNAMICS"
+"ARAD"
+, "TIMISOARA"
+, "SIBIU"
+, "FAGARUS"
+, "RIMNICU VILCEA"
+, "PITESTI"
+, "BUCHAREST"
+, "LUGOJ"
+, "ORZICENI"
+, "HIRSOVA"
 };
 
-int minDistance(int distance[],bool splset[])
+int findnextsource(int dis[],bool s[])
 {
-    int min=I,minposition;
+    int minvalue=I,minposition;
     for(int i=0;i<V;i++)
     {
-        if(!splset[i] && distance[i]<min)
+        if(!s[i] && dis[i]<minvalue)
         {
-            min=distance[i];
+            minvalue=dis[i];
             minposition=i;
         }
     }
     return minposition;
 }
 
-void print(int distance[],int VV)
+void print(int dis[],int VV)
 {
     cout<<"The Shortest Distance from Source is : \n ";
     for(int i=0;i<VV;i++)
     {
-        cout<<i<<"   -   "<<distance[i]<<endl;
+        cout<<i<<"   -   "<<dis[i]<<endl;
     }
 }
 
 void shortestpathfinder(int graph[V][V],int src)
 {
-    int distance[V],path[V];
-    bool splset[V];
+    int dis[V],path[V];
+    bool s[V];
     for(int i=0;i<V;i++)
     {
-        distance[i]=I;
-        splset[i]=false;
+        dis[i]=I;
+        s[i]=false;
     }
-    distance[src]=0;
+    dis[src]=0;
 
     for(int i=0;i<V-1;i++)
     {
-        int u=minDistance(distance,splset);
-        splset[u]=true;
+        int u=findnextsource(dis,s);
+        s[u]=true;
 
         for(int v=0;v<V;v++)
         {
-            if((!splset[v]) && graph[u][v] && (distance[u]+graph[u][v])<distance[v])
+            if((!s[v]) && graph[u][v] && (dis[u]+graph[u][v])<dis[v])
             {
-                  distance[v]=distance[u]+graph[u][v];
+                  dis[v]=dis[u]+graph[u][v];
                   path[v]=u;
             }
         }
     }
-    //print(distance,V);
+    //print(dis,V);
 int a[V][V],pw;
 
 for(int i=0;i<V;i++)
@@ -90,6 +92,7 @@ for(int i=0;i<V;i++)
 for(int i=0;i<V;i++)
 {
     stack<int> p;
+    Sleep(2000);
     cout<<" TO "<<str[i]<<" : \n";
     for(int j=0;a[i][j]!=999;j++)
     {
@@ -114,25 +117,40 @@ cout<<endl<<endl;
 
 int main()
 {
+    cout<<"";
      int graph[V][V] = {
-                      {0, 8, 0, 2, 7, 0, 0, 0, 0, 0},
-                      {8, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-                      {0, 1, 0, 3, 0, 4, 0, 0, 0, 0},
-                      {2, 0, 3, 0, 4, 5, 0, 0, 0, 0},
-                      {7, 0, 0, 4, 0, 2, 3, 1, 0, 0},
-                      {0, 0, 4, 5, 2, 0, 0, 9   , 0, 0},
-                      {0, 0, 0, 0, 3, 0, 0, 6, 5, 0},
-                      {0, 0, 0, 0, 1, 9, 6, 0, 4, 3},
-                      {0, 0, 0, 0, 0, 0, 5, 4, 0, 2},
-                      {0, 0, 0, 0, 0, 0, 0, 3, 2, 0},
+                    // 1  2  3  4  5  6  7  8  9  10
+                      {0, 8, 0, 2, 7, 0, 0, 0, 0, 0},//1
+                      {8, 0, 1, 0, 0, 0, 0, 0, 0, 0},//2
+                      {0, 1, 0, 0, 0, 4, 0, 0, 0, 0},//3
+                      {2, 0, 0, 0, 4, 0, 0, 0, 0, 0},//4
+                      {7, 0, 0, 4, 0, 2, 3, 1, 0, 0},//5
+                      {0, 0, 4, 0, 2, 0, 0, 9, 0, 0},//6
+                      {0, 0, 0, 0, 3, 0, 0, 6, 5, 0},//7
+                      {0, 0, 0, 0, 1, 9, 6, 0, 4, 3},//8
+                      {0, 0, 0, 0, 0, 0, 5, 4, 0, 2},//9
+                      {0, 0, 0, 0, 0, 0, 0, 3, 2, 0},//10
                        };
     int i;
-    cout<<"In which Section are you in : \n";
-    cout<<"0 NEWSSTAND \n1 MATHS\n2 REFERENCE STACK\n3 FICTION\n4 BIOLOGY\n5 GENERAL KNOWLEDGE\n6 PHYSICS\n";
-    cout<<"7 CHEMISTRY\n8 AERONAUTICS\n9 THERMODYNAMICS\n";
+//    system("COLOR C");
+
+//
+    system("COLOR F");
+    system("cls");
+
+    cout<<"\n\n                                                 MAP++\n\n";
+    cout<<"IN WHICH PLACE ARE YOU IN : \n\n";
+
+    cout<<"1 ARAD \n2 TIMISOARA\n3 SIBIU\n4 FAGARUS\n5 RIMNICU VILCEA\n6 PITESTI\n7 BUCHAREST\n8 LUGOJ\n9 ORZICENI\n10 HIRSOVA\n";
     cin>>i;
     system("cls");
-    cout<<str[i]<<" TO ALL MAP\n\n\n";
-    shortestpathfinder(graph,i);
+    cout<<"Finding the shortest path to save time ;)...";
+Sleep(3000);
+
+    system("cls");
+    cout<<"\n\nSHORTEST DISTANCE AND PATH FROM "<<str[i-1]<<" TO ALL OTHER CITIES \n\n\n";
+Sleep(2000);
+
+    shortestpathfinder(graph,i-1);
     return 0;
 }
